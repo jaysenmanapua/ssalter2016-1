ea971bbb2963c49869f9eb6065973075091261b4
jaysenmanapua <jaysenallen14@gmail.com>
2016-11-15 08:34:28 -1000

    Strategy Code

--- team1.py
+++ team1.py
@@ -7,8 +7,8 @@
 ####
 
 team_name = 'Jaysen and Collin' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'The name the team gives to this strategy =)'
+strategy_description = 'How does this strategy decide???'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
